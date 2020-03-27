3865af01a74460e6862967d873f3d08a71cd1226
anhhuy980413 <1653030@student.hcmus.edu.vn>
2020-03-27 14:58:23 +0700

    change flow get data (not get iframe)

--- js/crawl.js
+++ js/crawl.js
@@ -1,99 +1,91 @@
 $(document).ready(function () {
   $('#data').dataTable();
+  findData();
 });
 
 let regexGetHeaderURL = /(http){1}.*;{1}/;
 let regexGetPort = /(65){1}[0-9]*/;
 
-function checkPort(url, projectName){
-  return new Promise((resolve, reject)=>{
+
+function findData() {
+  // let table = $('#data').dataTable();
+  // table.fnClearTable();
+  // let projectName = $("#search-field").val();
+  // let data = localStorage.getItem(projectName);
+  // data.replace(/ /g,'');
+  // let listServer = data.split(',')
+  listServer = ['10.30.80.16'];
+  projectName = "zadmin"
+  listServer.forEach(server => {
+    getPortOfProject(server, projectName).then((allProject) => crawlData(allProject, projectName, server));
+  })
+}
+
+
+function getPortOfProject(server, projectName) {
+  let port = 65000;
+  return new Promise((resolve, reject) => {
     $.ajax({
       type: 'GET',
-      url: url,
+      url: 'http://' + server + ':65000/sc',
       success: function (resp) {
         let html = $(resp);
-        let allTd = html.find("tbody tr td");
-        if($(allTd[0]).text()==projectName){
-          let urlWithPort = $(allTd[2]).children().first().attr('href');
-          let port = regexGetPort.exec(urlWithPort);
-          resolve(port)
+        let allTr = html.find("tbody tr iframe");
+        let allProject = [];
+        for (i = 0; i < allTr.length; i++) {
+          let domain = allTr[i].src.replace(/bi.*/g, 'st');
+          allProject.push(domain);
         }
-        else{
-          reject(65000);
-        }
+        resolve(allProject, projectName, server);
       },
       error: function (data) {
+        console.log("error", data);
+        reject(65000);
       }
     });
   });
 }
 
-function getPortOfProject(server, projectName){
-let port=65000;
-return new Promise((resolve, reject) => {
-  $.ajax({
-    type: 'GET',
-    url: 'http://'+server + ':65000/sc',
-    success: function (resp) {
-      let html = $(resp);
-      let allTr = html.find("tbody tr");
-      for(i = 0 ; i < allTr.length; i++){
-        let domain  = regexGetHeaderURL.exec(allTr[i].innerHTML);
-        checkPort(domain[0], projectName).then(data => {port = data[0];resolve(port)}).catch(data => {});
+function crawlData(url, projectName, server) {
+    for (const index in url) {
+      if (url.hasOwnProperty(index)) {
+        console.log(url[index])
+        console.log(projectName)
+        $.ajax({
+          type: 'GET',
+          url: url[index],
+          success: function (resp) {
+            let html = $(resp);
+            curProjectName = html.find("h3")[0].innerHTML;
+            if (projectName == curProjectName) {
+              console.log('hello')
+              tbody = html.find("tbody");
+              let profiler = $(tbody[0]).find('tr');
+              let table = $('#data').dataTable();
+              for (i = 0; i < profiler.length; i++) {
+                $(profiler[i]).children().first().text(server)
+                table.fnAddData([
+                  $($(profiler[i]).children()[0]).text(), //server
+                  $($(profiler[i]).children()[1]).text(), //name
+                  $($(profiler[i]).children()[2]).text(), //totalReq
+                  $($(profiler[i]).children()[3]).text(), //pendingReq
+                  $($(profiler[i]).children()[4]).text(), //TotalTimeProc
+                  $($(profiler[i]).children()[5]).text(), //LastTmProc
+                  $($(profiler[i]).children()[6]).text(), //ProcRate
+                  $($(profiler[i]).children()[7]).text()  //ReqRate
+                ]);
+              }
+              console.log($(profiler[0]).children()[0])
+            }
+          },
+          error: function (data) {
+            console.log("error", data);
+          }
+        });
       }
-    },
-    error: function (data) {
-        console.log("error", data);
-        reject(65000);
     }
-  });
-});
 }
 
-function crawlData(url,server){
-  $.ajax({
-    type: 'GET',
-    url: url,
-    success: function (resp) {
-      let html = $(resp);
-      let table = $('#data').dataTable();
-      tbody = html.find("tbody");
-      let profiler = $(tbody[0]).find('tr');
-      for(i = 0 ; i < profiler.length ; i++){
-        $(profiler[i]).children().first().text(server)
-        table.fnAddData( [
-          $($(profiler[i]).children()[0]).text(),
-          $($(profiler[i]).children()[1]).text(),
-          $($(profiler[i]).children()[2]).text(),
-          $($(profiler[i]).children()[3]).text(),
-          $($(profiler[i]).children()[4]).text(),
-          $($(profiler[i]).children()[5]).text(),
-          $($(profiler[i]).children()[6]).text(),
-          $($(profiler[i]).children()[7]).text()  
-        ]);
-      }
-      console.log($(profiler[0]).children()[0])
-    },
-    error: function (data) {
-      console.log("error", data);
-    }
-  });
-}
-
-
-function findData(){
-  let table = $('#data').dataTable();
-  table.fnClearTable();
-  let projectName = $("#search-field").val();
-  let data = localStorage.getItem(projectName);
-  data.replace(/ /g,'');
-  let listServer = data.split(',')
-  listServer.forEach(server=>{
-    getPortOfProject(server,projectName).then(port => {crawlData('http://'+server+":"+port+"/st", server);});
-  })
-}
-
-function saveToStorage(projectName, listHost){
+function saveToStorage(projectName, listHost) {
   localStorage.setItem(projectName, listHost);
 }
-
