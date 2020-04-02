ccf3c37b22169db9decfd39aa21d207205f04df6
anhhuy980413 <1653030@student.hcmus.edu.vn>
2020-04-02 23:11:28 +0700

    v1 work for each

--- js/crawl.js
+++ js/crawl.js
@@ -1,8 +1,3 @@
-$(document).ready(function () {
-  initDB();
-  findData();
-  // window.setInterval(()=>{findData()}, 60000);
-});
 
 let regexGetHeaderURL = /(http){1}.*;{1}/;
 let regexGetPort = /(65){1}[0-9]*/;
@@ -12,6 +7,7 @@ let firstname, email, id;
 let profilerIndexedDB;
 let objName = 'profiler';
 let keyPath = 'ts';
+
 function initDB() {
   profilerIndexedDB = new ProfilerIndexedDB(INDEXED_DB_NAME);
   profilerIndexedDB.open(
@@ -19,11 +15,12 @@ function initDB() {
   );
 }
 
-function findData() {
-  listServer = ['10.30.80.16'];
-  projectName = "zadmin"
+function findData(projectName, listServer, optionDisplay, numberServer) {
   listServer.forEach(server => {
-    getPortOfProject(server, projectName).then((allProject) => crawlData(allProject, projectName, server));
+    getPortOfProject(server, projectName)
+    .then((allProject) => crawlData(allProject, projectName, server))
+    .then((fullyData) => displayData(fullyData, optionDisplay, numberServer))
+    .catch(data =>{console.log('loi cmnr', data)});
   });
 
 }
@@ -56,47 +53,67 @@ function getPortOfProject(server, projectName) {
 }
 
 function crawlData(url, projectName, server) {
+  return new Promise( (resolve, reject) => {
+    let FullyData = [];
+    let listAjax = [];
     for (const index in url) {
       if (url.hasOwnProperty(index)) {
-        $.ajax({
-          type: 'GET',
-          url: url[index],
-          success: function (resp) {
-            let html = $(resp);
-            curProjectName = html.find("h3")[0].innerHTML;
-            if (projectName == curProjectName) {
-              tbody = html.find("tbody");
-              let profiler = $(tbody[0]).find('tr');
-              let table = $('#data').dataTable();
-              for (i = 0; i < profiler.length; i++) {
-                $(profiler[i]).children().first().text(server)
-                table.fnAddData([
-                  $($(profiler[i]).children()[0]).text(), //server
-                  $($(profiler[i]).children()[1]).text(), //nameProject
-                  $($(profiler[i]).children()[2]).text(), //totalReq
-                  $($(profiler[i]).children()[3]).text(), //pendingReq
-                  $($(profiler[i]).children()[4]).text(), //TotalTimeProc
-                  $($(profiler[i]).children()[5]).text(), //LastTmProc
-                  $($(profiler[i]).children()[6]).text(), //ProcRate
-                  $($(profiler[i]).children()[7]).text()  //ReqRate
-                ]);
-                saveDataProfilerToIndexDB($(profiler[i]).children());
-              }
-            }
-          },
-          error: function (data) {
-            console.log("error", data);
-          }
-        });
+        listAjax.push(callAjax(url[index],projectName,server));
       }
     }
+    Promise.all(listAjax).then(data => {
+      for(i in data){
+        if(data[i].length >0){
+          resolve(data[i])
+        }
+      }
+      reject();
+    }).catch(() => {reject()});
+  });
 }
 
 function saveToStorage(projectName, listHost) {
   localStorage.setItem(projectName, listHost);
 }
 
-
+function callAjax(url, projectName,server){
+  return new Promise((resolve, reject)=>{
+  $.ajax({
+    type: 'GET',
+    url: url,
+    success: async function (resp) {
+      let FullyData =[];
+      let html = $(resp);
+      curProjectName = html.find("h3")[0].innerHTML;
+      if (projectName == curProjectName) {
+        tbody = html.find("tbody");
+        let profiler = $(tbody[0]).find('tr');
+        for (i = 0; i < profiler.length; i++) {
+          $(profiler[i]).children().first().text(server)
+          FullyData.push({
+            server: $($(profiler[i]).children()[0]).text(),
+            nameProject: $($(profiler[i]).children()[1]).text(),
+            totalReq: $($(profiler[i]).children()[2]).text(),
+            pendingReq: $($(profiler[i]).children()[3]).text(),
+            TotalTimeProc: $($(profiler[i]).children()[4]).text(),
+            LastTmProc: $($(profiler[i]).children()[5]).text(),
+            ProcRate: $($(profiler[i]).children()[6]).text(),
+            ReqRate: $($(profiler[i]).children()[7]).text(),
+          })
+          saveDataProfilerToIndexDB($(profiler[i]).children());
+        }
+        resolve(FullyData);
+      }else{
+        resolve(FullyData);
+      }
+    },
+    error: function (data) {
+      resolve();
+      console.log("error", data);
+    }
+  });
+})
+}
 function openDBSuccess() {
   console.log('open db success');
 }
@@ -125,4 +142,92 @@ function saveDataProfilerToIndexDB(dataCrawl){
     event => console.log('add success', event),
     error => console.log('add error', error)
   );
+}
+
+
+
+function displayData(fullyData, optionDisplay, numberServer){
+  let table = $('#data').dataTable();
+  if(optionDisplay == 2){//avg
+    dataSum = sumDataByProject(fullyData);
+    for(index in dataSum){
+      table.fnAddData([
+        dataSum[index].server,
+        dataSum[index].nameProject,
+        dataSum[index].totalReq/numberServer,
+        dataSum[index].pendingReq/numberServer,
+        dataSum[index].TotalTimeProc/numberServer,
+        dataSum[index].LastTmProc/numberServer,
+        dataSum[index].ProcRate/numberServer,
+        dataSum[index].ReqRate/numberServer,
+      ]);
+    }
+  }else if(optionDisplay ==1){ //sum
+    dataSum = sumDataByProject(fullyData);
+    dataSum = sumDataByProject(fullyData);
+    for(index in dataSum){
+      table.fnAddData([
+        dataSum[index].server,
+        dataSum[index].nameProject,
+        dataSum[index].totalReq,
+        dataSum[index].pendingReq,
+        dataSum[index].TotalTimeProc,
+        dataSum[index].LastTmProc,
+        dataSum[index].ProcRate,
+        dataSum[index].ReqRate,
+      ]);
+    }
+  }else {//each
+    console.log(fullyData.length)
+    for(index in fullyData){
+      console.log('inhear');
+      table.fnAddData([
+        fullyData[index].server,
+        fullyData[index].nameProject,
+        fullyData[index].totalReq,
+        fullyData[index].pendingReq,
+        fullyData[index].TotalTimeProc,
+        fullyData[index].LastTmProc,
+        fullyData[index].ProcRate,
+        fullyData[index].ReqRate,
+      ]);
+    }
+
+  }
+}
+
+function sumDataByProject(fullyData) {
+  var result = fullyData.reduce(function (acc, val) {
+    var o = acc.filter(function (obj) {
+      return obj.nameProject == val.nameProject;
+    }).pop() || {
+        server: val.server,
+        nameProject: val.nameProject,
+        totalReq: 0,
+        pendingReq: 0,
+        TotalTimeProc: 0,
+        LastTmProc: 0,
+        ProcRate: 0,
+        ReqRate: 0,
+      };
+
+    o.totalReq += val.totalReq;
+    o.pendingReq += val.pendingReq;
+    o.TotalTimeProc += val.TotalTimeProc;
+    o.LastTmProc += val.LastTmProc;
+    o.ProcRate += val.ProcRate;
+    o.ReqRate += val.ReqRate;
+    acc.push(o);
+    return acc;
+  }, []);
+  result = getUnique(result, 'nameProject')
+  return result;
+}
+
+function getUnique(arr, comp) {
+  const unique = arr
+    .map(e => e[comp])
+    .map((e, i, final) => final.indexOf(e) === i && i)
+    .filter(e => arr[e]).map(e => arr[e]);
+  return unique;
 }
--- js/main.js
+++ js/main.js
@@ -1,15 +1,114 @@
 $(document).ready(function () {
+    initDB(); //init IndexedDB
+    initSearchProject(); //init project from localstorage
+    initSearchData(); //search in table data
+});
+
+
+$('#modal-edit-project').on('show.bs.modal', function() {
+    $('#listProject').empty();
+    let projects = [];
+    let servers = {};
+    const storage = {...localStorage};
+    for( let projectName in storage){
+        projects.push({'projectName': projectName});
+        servers[projectName] = storage[projectName].split(',');
+    }
+    let data = {projects: projects};
+    let templateOwner = $("#itemManageServer").html();
+    let projectHTML = Mustache.render(templateOwner, data);
+    let buttonTemplate = $("#buttonNewProject").html();
+    let buttonHTML = Mustache.render(buttonTemplate);
+    $('#listProject').append(projectHTML);
+    $('#listProject').append(buttonHTML);
+
+    for (let projectName in storage) {
+        const project = projectName;
+        const listServer = servers[projectName];
+        let dataServer = [];
+        for (const i in listServer) {
+            dataServer.push({ server: listServer[i] });
+        }
+        $('#input-server-ips-' + project).selectize({
+            create: true,
+            persist: false,
+            maxItems: null,
+            valueField: 'server',
+            labelField: 'server',
+            searchField: ['server'],
+            options: dataServer,
+            delimiter: ',',
+            render: {
+                item: function (item) {
+                    return '<div>' +
+                        (item.server ? '<span class="name">' + item.server + '</span>' : '') +
+                        '</div>';
+                }
+            },
+            onInitialize: function () {
+                var $select = $("#input-server-ips-" + project).selectize();
+                var selectize = $select[0].selectize;
+                selectize.setValue(listServer);
+            },
+        });
+    }
+});
+
+$(document).on('click', '#newProject', function() {
+    $("#newProjectLi").remove();
+
+    let newProjectTemplate = $("#liDontIdNewProject").html();
+    let newProjectHTML = Mustache.render(newProjectTemplate);
+    $('#listProject').append(newProjectHTML);
+
+    $('#newServer').selectize({
+        create: true,
+        persist: false,
+        maxItems: null,
+        delimiter: ','
+        
+    });
+
+});
+
+$("#btnSaveManageProject").click(()=>{
+    //save update current project
+    let currentProject = [];
+    $("label[name=project]").each(function(){
+        currentProject.push($(this).text());
+    });
+    for(i in currentProject){
+        let value = $("#input-server-ips-"+currentProject[i]).val().split(',');
+        localStorage.setItem(currentProject[i], value);
+    }
+    
+    const newProject = $("#newProjectValue").val();
+    const newServer = $("#newServer").val();
+    if(newProject!= undefined && newProject.length > 0 && newServer != undefined && newServer.length > 0){
+        const listNewServer = newServer.split(',');
+        localStorage.setItem(newProject, listNewServer);
+    }
+    const storage = {...localStorage};
+    let option = [];
+    for(project in storage){
+        option.push({name:project, servers: storage[project]});
+    }
+    $('#intput-project-search').selectize()[0].selectize.addOption(option);
+})
+
+function initSearchProject(){
+    const storage = {...localStorage};
+    let option = [];
+    for(project in storage){
+        option.push({name:project, servers: storage[project]});
+    }
+
     $('#intput-project-search').selectize({
         create: false, 
         searchField: ['name', 'servers'],
         valueField: 'name',
         labelField: 'name',
-        options: [
-            { name: 'zadmin', servers: "10.30.80.16,10.30.58.126" },
-            { name: 'authen-pc', servers: "10.30.80.16,10.30.58.126" },
-            { name: 'zalo-webpc-api-profile', servers: "10.30.80.16,10.30.58.126" },
-            { name: 'zalo-webpc-api-friend', servers: "10.30.80.16,10.30.58.126" }
-        ],
+        options: option,
         render: {
             item: function (item, escape) {
                 return '<div>' +
@@ -22,12 +121,22 @@ $(document).ready(function () {
                     '<div class="caption">' + escape(item.servers) + '</div>' +
                     '</div>';
             }
+        },
+        onChange: function () {
+            optionView = $('input[name="opt-type"]:checked').val();
+            const projectName = $('#intput-project-search').val();
+            const listServer = storage[projectName].split(',');
+            findData(projectName, listServer,optionView,listServer.length );
         }
     });
-
-    $('.input-server-ips').selectize({
-        delimiter: ',',
-        persist: false,
-        create: true
+}
+function initSearchData(){
+    const table = $('#data').DataTable({
+        // searching: false,
+        paging: false,
+        info: false,
     });
-});
+    $('#filter').on( 'keyup change clear', function () {
+        table.search(this.value).draw();
+    } );
+}
